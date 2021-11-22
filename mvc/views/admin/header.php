b31bc6245a86e0b84700308396a482bac344ca69
Bao Nguyen Thien <nguyenthienbao0402@gmail.com>
2021-11-22 11:13:00 +0700

    add account button

--- mvc/views/admin/header.php
+++ mvc/views/admin/header.php
@@ -41,10 +41,15 @@
                         <a class="nav-link disabled">Disabled</a>
                     </li> -->
                 </ul>
-                <div class="d-flex account-area">
-                    <a href="#" class="btn btn-light">Đăng nhập</a>
-                    <a href="#" class="btn btn-primary">Đăng ký</a>
-
+                <div class="dropdown">
+                    <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+                        Hi, Bảo
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="dropdownMenu2">
+                        <li><button class="dropdown-item" type="button">Action</button></li>
+                        <li><button class="dropdown-item" type="button">Another action</button></li>
+                        <li><button class="dropdown-item" type="button">Something else here</button></li>
+                    </ul>
                 </div>
             </div>
         </div>
